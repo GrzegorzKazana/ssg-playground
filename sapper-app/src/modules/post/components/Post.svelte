3a232894d11b389e11a8bdc83f2c00703695f4d3
Grzegorz Kazana <kazana.grzegorz@gmail.com>
2020-03-21 10:30:40 +0100

    feat(sapper): dynamic post pages

---
+++ sapper-app/src/modules/post/components/Post.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let post;
+</script>
+
+<article>
+    <h2 class="title">{post.title}</h2>
+    <p class="text">{post.body.repeat(10)}</p>
+</article>
+
+<style lang="scss">
+    .title {
+        text-align: center;
+        margin: 2rem auto 3rem auto;
+    }
+
+    .text {
+        text-align: justify;
+    }
+</style>
---
+++ sapper-app/src/routes/post/[pid].svelte
@@ -0,0 +1,27 @@
+<script context="module">
+    import { postsUrl } from '../../config/variables';
+
+    export function preload(page, session) {
+        const { pid } = page.params;
+
+        return this.fetch(`${postsUrl}/${pid}`)
+            .then(res => res.json())
+            .then(post => ({
+                post,
+            }));
+    }
+</script>
+
+<script>
+    import Layout from '../../common/components/Layout.svelte';
+    import Post from '../../modules/post/components/Post.svelte';
+
+    export let post;
+</script>
+
+<Layout>
+    <Post {post} />
+    <!-- <ClientOnly>
+        <CommentContainer postId={post.id}></CommentContainer>
+      </ClientOnly> -->
+</Layout>
